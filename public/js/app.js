4b5e03c06cb599b14db73e6975e030b9d27500a5
Jose Ignacio <joserobleda@gmail.com>
2014-12-17 22:19:37 +0100

    resolve members and tasks

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,40 @@ var app = angular.module('dokifyApp', [
 ]);
 
 app.run(function ($rootScope) {
-	console.log('listo!');
+
 });
 
 app.config(['$routeProvider', function($routeProvider) {
 
+	$routeProvider
+		.when('/', {
+			templateUrl: '/html/team.html',
+			controller: 'TeamCtrl',
+			resolve: {
+				members: function ($q, $http) {
+					return $http.get('/members.json').then(function (res) {
+						var members = res.data;
+						var promises = [];
+
+						angular.forEach(members, function (member) {
+							var promise = $http.get(member.tasks).then(function (res) {
+								var tasks = res.data;
+								member.tasks = tasks;
+							});
+
+							promises.push(promise);
+						});
+
+						var promise = $q.all(promises).then(function () {
+							return members;
+						});
+
+						return promise;
+					});
+				}
+			}
+		})
+		.when('/:member', {
+			templateUrl: '/html/member.html'
+		});
 }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,5 @@
 var appControllers = angular.module('appControllers', []);
+
+appControllers.controller('TeamCtrl', function ($scope, members) {
+	$scope.members = members;
+});
