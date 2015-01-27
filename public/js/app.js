112abf385318e4813ce54c488d7fae5f37c486e1
Jose Ignacio <joserobleda@gmail.com>
2015-01-27 17:36:49 +0100

    clean things

--- public/js/app.js
+++ public/js/app.js
@@ -7,30 +7,7 @@ app.run(function ($rootScope) {
 
 });
 
-// app.factory('tasks', [function () {
-// 	var db = new PouchDB('tasks');
-
-
-// 	var changes = db.changes({
-// 	  live: true
-// 	}).on('change', function(change) {
-// 		console.log('changes', change);
-// 	});
-
-// 	PouchDB.sync('tasks', 'http://localhost:5984/tasks', {live: true});
-
-// 	return db;
-// }]);
-
-app.factory('MembersDB', [function () {
-	var db = new PouchDB('members');
-
-	PouchDB.sync('members', 'http://joserobleda.iriscouch.com//members', {live: true});
-	return db;
-}]);
-
 app.config(['$routeProvider', function($routeProvider) {
-
 	$routeProvider
 		.when('/', {
 			templateUrl: '/html/team.html',
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,31 +1,5 @@
 var appControllers = angular.module('appControllers', []);
 
-appControllers.controller('TeamCtrl', function ($scope, MembersDB) {
+appControllers.controller('TeamCtrl', function ($scope) {
 
-	$scope.getMembers = function () {
-		console.log('getMembers');
-		MembersDB.allDocs({include_docs: true}).then(function (response) {
-			var members = [];
-
-			angular.forEach(response.rows, function(member) {
-				this.push(member.doc);
-			}, members);
-
-			$scope.$apply(function () {
-				$scope.members = members;
-			});
-		});
-	};
-
-	$scope.createMember = function () {
-		var member = {
-			name: $scope.newMember
-		};
-
-		MembersDB.post(member).then(function () {
-			$scope.getMembers();
-		})
-	};
-
-	$scope.getMembers();
 });
